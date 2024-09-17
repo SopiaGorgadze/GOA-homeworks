6ca5c858f4a1df4631154d0f7df90b9dbae76024
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-09-17 16:33:44 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,23 @@
+num1 = 10
+num2 = 20
+  
+
+#addition
+
+print(str(num1)+  " + " +  str(num2) + " = " + str(int(num1)+ int(num2)))
+
+#multiplication
+
+print(str(num1)+  " * " +  str(num2) + " = " + str(int(num1) * int(num2)))
+
+#division
+
+print(str(num1)+  " / " +  str(num2) + " = " + str(int(num1) / int(num2)))
+
+#subtraction
+
+print(str(num2)+  " - " +  str(num1) + " = " + str(int(num2) - int(num1)))
+
+
+
+
