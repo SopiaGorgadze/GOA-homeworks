f84310310892489d93b25f55a0076a75698df840
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-09-17 16:53:01 +0400

    Delete day 1 directory

--- day 1/lesson1.py
+++
@@ -1,8 +0,0 @@
-name = "sopia"
-surname = "gorgadze"
-age = "16"
-height = "1.60"
-weight = "45"
-print("chemi sakhelia:", name, surname, "chemi asakia:", age, "chemi simaghlea:", height, "chemi wonaa:", weight,)
-
-  
