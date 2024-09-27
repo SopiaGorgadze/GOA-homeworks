486486d0430dadea79ba34a1685cab2b5d09122c
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-09-27 07:15:20 +0400

    commit

---
+++ level06/homework6/hw.py
@@ -0,0 +1,6 @@
+#1) შექმენი ორი input შეადარე ორი რიცხვი და დაბეჭდე შედეგი.
+
+num1 = int(input("please enter a number: "))
+num2 = int(input("please enter another number: "))
+
+print(num1 == num2)
---
+++ level06/homework6/hw3.py
@@ -0,0 +1,4 @@
+#3) მომხმარებელს შემოატანინე input შეამოწმე, არის თუ არა რიცხვი 50-ზე ნაკლები.
+
+num = int(input("please enter a number: "))
+print(num < 50)
