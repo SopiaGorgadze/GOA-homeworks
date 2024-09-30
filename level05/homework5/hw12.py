94f5e346d2ec6380f7e393a129699f01e4343bc6
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-09-30 18:24:12 +0400

    commit

--- level05/homework5/hw12.py
+++ level05/homework5/hw12.py
@@ -1,13 +1,4 @@
-#მომხმარებელს შემოატანინეთ ორი რიცხვი შემდეგ კი ამ ორი რიცხვისგან შეადგინეთ ოთხი მათემატიკური მოქმედება, დამატება გამოკლება გამრავლება და გაყოფა
-num1 = int(input("enter any number: "))
-num2 = int(input("enter another number: "))
-
-result = num1 * num2
-result2 = num1 - num2
-result3 = num1 + num2
-result4 = num1 / num2
-
-print(result)
-print(result2)
-print(result3)
-print(result4)
+#მომხმარებელს შეეკითხეთ მისი ასაკი შემდეგ კი დაბეჭდეთ 10 წლის შემდეგ რამდენი წლის იქნება
+num1 = int(input("please enter your age: "))
+result = num1 + 10
+print("after 10 years you will be" + " " + str(result) + " " + "years old")
--- level05/homework5/hw13.py
+++
@@ -1,4 +0,0 @@
-#მომხმარებელს შეეკითხეთ მისი ასაკი შემდეგ კი დაბეჭდეთ 10 წლის შემდეგ რამდენი წლის იქნება
-num1 = int(input("please enter your age: "))
-result = num1 + 10
-print("after 10 years you will be" + " " + str(result) + " " + "years old")
