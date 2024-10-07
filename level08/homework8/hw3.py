7fd1c3bfe3dc8179a59118c4e49f683ac6b6c9eb
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-10-07 04:49:39 +0400

    commit

--- level08/homework8/hw3.py
+++ level08/homework8/hw3.py
@@ -4,5 +4,7 @@ user_num = int(input("please enter your number: "))
 
 if user_num > 0:
     print("your number is positive")
+elif user_num == 0:
+    print("your num is 0")
 else:
     print("your number is negative")
