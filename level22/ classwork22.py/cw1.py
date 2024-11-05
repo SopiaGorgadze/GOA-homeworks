431e3cfb02e089938b1d38abc7002139eb48bd2d
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-11-05 23:43:55 +0400

    commit

---
+++ level22/ classwork22.py/cw1.py
@@ -0,0 +1,9 @@
+names = ["sopia", "vano", "lizi" ,"mari"]
+
+print(names[1:4])
+
+
+# reverse
+print(names[:: -1])   
+
+# slicing doesnt work on ints
---
+++ level22/ classwork22.py/cw2.py
@@ -0,0 +1,36 @@
+# 1) მომხაარებელ შემოატანინეთ სახელი და გვარი ეს სახელი და გვარი გააერთიანეთ და შეაუბრუნეთ
+
+
+name = input("enter your name: ")
+surname = input("enter your lastname :")
+
+both = name + surname
+
+print(both[::-1])
+
+
+# slicing
+names = ["vano", "sopia", "luka" ]
+print(names[1:3])
+print(names[::2])   #gamotova 2
+
+# len() - length
+print(len(name))
+
+# append() - add elements to a list
+
+names = ["vano", "sopia", "luka" ]
+
+
+# dot notation
+names.append(5)
+
+print(names)
+
+
+
+# insert() 
+# pop()
+
+ 
+
