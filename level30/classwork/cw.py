4fef4ff0a1b32b9ba0917b16d8bd77448a4f784e
SopiaGorgadze <sopia.gorgadze@icloud.com>
2024-11-23 04:59:21 +0400

    commit

---
+++ level30/classwork/cw.py
@@ -0,0 +1,25 @@
+# function
+
+
+from turtle import *
+
+
+# def - define - შექმნა
+
+def square():
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+     
+    
+
+square()
+ 
+ 
+
+
+exitonclick()
